1c2ae416fa108cdefa8b655e3b88179170c46545
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2022-11-11 17:54:18 -0800

    Re-created project

---
+++ Code/app/src/main/java/com/cmpt362/fitcheck/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cmpt362.fitcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
