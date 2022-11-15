2fcb530d541edcb7f70ade082000395e71d2a509
Janet Ahn <janet@formswim.com>
2022-11-15 12:58:43 -0800

    add calendar view to the CalendarFragment.kt

--- Code/app/src/main/java/com/cmpt362/fitcheck/ui/calendar/CalendarFragment.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/calendar/CalendarFragment.kt
@@ -1,13 +1,15 @@
 package com.cmpt362.fitcheck.ui.calendar
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.CalendarView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.cmpt362.fitcheck.databinding.FragmentCalendarBinding
+
 
 class CalendarFragment : Fragment() {
 
@@ -22,16 +24,21 @@ class CalendarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val calendarViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
+        val calendarViewModel = ViewModelProvider(this).get(CalendarViewModel::class.java)
 
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        calendarViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        calendarViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        val calendarView: CalendarView = binding.simpleCalendarView
+        val selectedDate: Long = calendarView.date
+        calendarView.date = selectedDate
+        calendarView.firstDayOfWeek = 1
+
+
         return root
     }
 
--- Code/app/src/main/java/com/cmpt362/fitcheck/ui/calendar/CalendarViewModel.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/calendar/CalendarViewModel.kt
@@ -1,5 +1,6 @@
 package com.cmpt362.fitcheck.ui.calendar
 
+import android.widget.CalendarView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
