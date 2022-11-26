4bfba0ff7b3dc85bfd359e46c93b74f662e91760
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2022-11-26 15:58:42 -0800

    Added Android Studio generated settings fragment and updated UI.

--- Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
@@ -1,35 +1,12 @@
 package com.cmpt362.fitcheck.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.cmpt362.fitcheck.databinding.FragmentSettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import com.cmpt362.fitcheck.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    private lateinit var settingsView: View
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        settingsView = binding.root
-        return settingsView
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
