013ebd78c43cf69459579ba4811751afe571e1a0
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2022-11-26 15:14:49 -0800

    Removed temporary code for settings, disabled screen rotation for all activities, and added settings dependency to build.gradle

--- Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
@@ -1,18 +1,11 @@
 package com.cmpt362.fitcheck.ui.settings
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.cmpt362.fitcheck.R
-import com.cmpt362.fitcheck.ui.authentication.LoginActivity
 import com.cmpt362.fitcheck.databinding.FragmentSettingsBinding
-import com.cmpt362.fitcheck.firebase.Firebase
 
 class SettingsFragment : Fragment() {
 
@@ -22,33 +15,17 @@ class SettingsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var settingsView: View
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textSettings
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        // Logging a user out when Logout button is clicked
-        binding.root.findViewById<Button>(R.id.logout_button).setOnClickListener(){
-            // Logic for logging a user out
-            Firebase.signOut()
-
-            // User logged out so end current activity and go back to Login activity
-            activity?.finish()
-            startActivity(Intent(requireContext(), LoginActivity::class.java))
-        }
-
-        return root
+        settingsView = binding.root
+        return settingsView
     }
 
     override fun onDestroyView() {
