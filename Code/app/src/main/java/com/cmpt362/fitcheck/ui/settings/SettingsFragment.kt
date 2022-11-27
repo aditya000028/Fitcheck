97b797f4b7a6cf892a752a71aad9c3ac7faa9132
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2022-11-26 16:14:33 -0800

    Added functional logout button to settings fragment

--- Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
@@ -1,12 +1,29 @@
 package com.cmpt362.fitcheck.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.cmpt362.fitcheck.R
+import com.cmpt362.fitcheck.firebase.Firebase
+import com.cmpt362.fitcheck.ui.authentication.LoginActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        initializePreferencesBehaviour()
+    }
+
+    private fun initializePreferencesBehaviour() {
+        val logoutPreference = findPreference<Preference>(getString(R.string.logout))
+        logoutPreference?.setOnPreferenceClickListener {
+            Firebase.signOut()
+            activity?.finish()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+
+            true
+        }
     }
 }
