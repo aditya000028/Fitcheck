fbee34d7b9e55c510d794e4dabc4ccce5f4bce42
alyssarusk <83485098+alyssarusk@users.noreply.github.com>
2022-11-27 02:30:26 -0800

    Set up Firebase to upload photos with metadata to cloud storage

--- Code/app/src/main/java/com/cmpt362/fitcheck/AddPhotoActivity.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/AddPhotoActivity.kt
@@ -7,32 +7,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
+import com.cmpt362.fitcheck.firebase.Firebase
 import java.io.File
 
 class AddPhotoActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
+    private lateinit var notesText: EditText
     private lateinit var tempImgUri: Uri
     private lateinit var tempImgFile: File
     private val tempImgFileName = "fitcheck_temp_img.jpg"
     private lateinit var cameraResult: ActivityResultLauncher<Intent>
+    private var photoTaken = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
         imageView = findViewById(R.id.outfitImage)
+        notesText = findViewById(R.id.notesText)
 
         tempImgFile = File(getExternalFilesDir(null), tempImgFileName)
         tempImgUri = FileProvider.getUriForFile(
             this, "com.cmpt362.fitcheck", tempImgFile
         )
+        photoTaken = 0
 
         // Display photo taken in imageView
         cameraResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
@@ -40,6 +45,7 @@ class AddPhotoActivity : AppCompatActivity() {
             if(result.resultCode == Activity.RESULT_OK){
                 val bitmap = Util.getBitmap(this, tempImgUri)
                 imageView.setImageBitmap(bitmap)
+                photoTaken = 1
 
             }
         }
@@ -58,9 +64,34 @@ class AddPhotoActivity : AppCompatActivity() {
      * When save button is click, save the upload and finish the activity
      */
     fun onSaveUpload(view: View){
-        Toast.makeText(this, R.string.save_message, Toast.LENGTH_SHORT).show()
-        // ToDo: Save photo to firebase
-        this.finish()
+        // Check that photo was taken
+        if (photoTaken == 1) {
+            // Get any notes from user
+            val notes = notesText.text.toString()
+
+            // Add photo and notes to cloud storage
+            val uploadTask = Firebase.addPhoto(tempImgUri, notes)
+
+            // Check that UploadTask was created
+            if (uploadTask != null) {
+                // Add listeners for file success, progress, and failure
+                uploadTask.addOnSuccessListener { taskSnapshot ->
+                    // If successful, update user and finish activity
+                    Toast.makeText(this, R.string.save_message, Toast.LENGTH_SHORT).show()
+                    this.finish()
+                    }.addOnProgressListener { taskSnapshot ->
+                            val progress = (100.0 * taskSnapshot.bytesTransferred) / taskSnapshot.totalByteCount
+                            println("Upload is $progress% done")
+                    }.addOnFailureListener { exception ->
+                        // On failure, inform user and output error
+                        println(exception)
+                        Toast.makeText(this, R.string.failure_message, Toast.LENGTH_SHORT).show()
+                    }
+            }
+        } else {
+            // Inform user that picture must be taken
+            Toast.makeText(this, R.string.take_picture_message, Toast.LENGTH_SHORT).show()
+        }
     }
 
     /**
