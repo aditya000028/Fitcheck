931368f55f72755c1f784c79092ebdb1a5aa66f7
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2022-11-27 20:16:33 -0800

    Created reset password activity along with ui

---
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/ResetPasswordActivity.kt
@@ -0,0 +1,17 @@
+package com.cmpt362.fitcheck.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.cmpt362.fitcheck.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reset_password_activity)
+    }
+
+    fun onSavePassword(view: View) {
+        
+    }
+}
