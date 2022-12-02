61debdd4daa6e878b8dfbed0a96db33ef7201c34
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2022-12-02 15:45:55 -0800

    Added ui component for daily reminder toggle and time

--- Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
+++ Code/app/src/main/java/com/cmpt362/fitcheck/ui/settings/SettingsFragment.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.cmpt362.fitcheck.R
 import com.cmpt362.fitcheck.firebase.Firebase
 import com.cmpt362.fitcheck.ui.authentication.LoginActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    private lateinit var logoutPreference: Preference
+    private lateinit var uploadTimePreference: Preference
+    private lateinit var dailyUploadReminderTimeToggle: SwitchPreferenceCompat
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
@@ -17,12 +22,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     private fun initializePreferencesBehaviour() {
-        val logoutPreference = findPreference<Preference>(getString(R.string.logout))
-        logoutPreference?.setOnPreferenceClickListener {
+        logoutPreference = findPreference(getString(R.string.logout))!!
+        logoutPreference.setOnPreferenceClickListener {
             Firebase.signOut()
             activity?.finish()
             startActivity(Intent(requireContext(), LoginActivity::class.java))
 
+            true
+        }
+
+        uploadTimePreference = findPreference(getString(R.string.daily_outfit_upload_reminder_time))!!
+        uploadTimePreference.summaryProvider = Preference.SummaryProvider<Preference> { preference ->
+            "test upload time"
+        }
+
+        dailyUploadReminderTimeToggle = findPreference(getString(R.string.daily_outfit_upload_reminder))!!
+        dailyUploadReminderTimeToggle.setOnPreferenceChangeListener { _, newValue ->
+            when (newValue as Boolean) {
+                true -> uploadTimePreference.isEnabled = true
+                false -> uploadTimePreference.isEnabled = false
+            }
             true
         }
     }
